e51014ec229a262dff891e778746ffc96e9d6d8f
Gabriel Urbain <gabrielurbain@gmail.com>
2018-10-17 10:15:35 +0200

    Add the last custom tracker developped in June

--- src/tigrillo_tracking/src/tigrillo_tracking/camshift.py
+++ src/tigrillo_tracking/src/tigrillo_tracking/camshift.py
@@ -1,5 +1,6 @@
 import numpy as np
-import cv2 as cv
+import cv2
+import copy
 
 
 class CamShift:
@@ -16,15 +17,15 @@ class CamShift:
         self.img_num = 0
 
         # Termination criteria (10 its or moving)
-        self.term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
+        self.term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
     def get_roi_hist(self, cam_shift):
 
         roi = cam_shift[self.r:self.r+self.h, self.c:self.c+self.w]
-        hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-        roi_hist = cv.calcHist([hsv_roi], [0], mask, [180],[0,180])
-        cv.normalize(roi_hist, roi_hist, 0, 255, cv.NORM_MINMAX)
+        hsv_roi =  cv2.cv2tColor(roi, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180],[0,180])
+        cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
         return roi_hist
 
@@ -34,11 +35,11 @@ class CamShift:
         if self.img_num == 0:
             self.roi_hist = self.get_roi_hist(img)        
 
-        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-        dst = cv.calcBackProject([hsv],[0], self.roi_hist, [0,180], 1)
+        hsv = cv2.cv2tColor(img, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv],[0], self.roi_hist, [0,180], 1)
 
         # apply meanshift to get the new location
-        self.ret, self.track_window = cv.CamShift(dst, self.track_window, self.term_crit)
+        self.ret, self.track_window = cv2.CamShift(dst, self.track_window, self.term_crit)
 
         # Increment image counter
         self.img_num += 1
@@ -55,10 +56,82 @@ class CamShift:
         elif col == "red":
             comp = (0, 0, 255)
 
-        pts = cv.boxPoints(self.ret)
+        pts = cv2.boxPoints(self.ret)
         pts = np.int0(pts)
-        img2 = cv.polylines(img, [pts], True, comp, 2)
+        img2 = cv2.polylines(img, [pts], True, comp, 2)
 
         return img2
 
 
+class CustomTracker:
+
+
+    def __init__(self, part="front"):
+
+        self.part = part
+
+        self.ret = None
+        self.img_num = 0
+        self.img_tracked = None
+
+    def process(self, img):
+
+        # Convert to HSV
+        img_hsv = copy.copy(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+        img_hsv[0:200, :] = 0
+
+        # mask of red
+        if self.part == "back":
+            mask_red = cv2.inRange(img_hsv, (150, 150, 150), (180, 255,255))
+            mask_red_2 = cv2.inRange(img_hsv, (0, 150, 150), (20, 255,255))
+            imask = (mask_red > 0) + (mask_red_2 > 0) # + (mask_blue > 0)
+
+        # mask of blue
+        if self.part == "front":
+            mask_blue = cv2.inRange(img_hsv, (85, 150, 150), (110, 255, 255))
+            imask = (mask_blue > 0)
+        
+        # slice the color and convert to gray
+        self.new = np.zeros_like(img, np.uint8)
+        self.new[imask] = img[imask]
+        self.img_grey = cv2.cvtColor(self.new, cv2.COLOR_BGR2GRAY)
+        thresh, img_bw = cv2.threshold(self.img_grey, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+        # erosion
+        kernel = np.ones((3, 3), np.uint8)
+        img_open = cv2.morphologyEx(img_bw, cv2.MORPH_OPEN, kernel)
+
+        # dilatation
+        self.img_dil = 255 - cv2.dilate(img_open, kernel, iterations=1)
+
+        # find contours
+        self.img_tracked, contours, hierarchy = cv2.findContours(self.img_dil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        best_ctn = None
+        best_ctn_val = 0
+        for i, cnt in enumerate(contours):
+            if 70 < cv2.contourArea(cnt) < 5000:
+                if cv2.contourArea(cnt) > best_ctn_val:
+                    best_ctn = cnt
+
+        if best_ctn is not None:
+            self.ret = cv2.minAreaRect(best_ctn)
+        else:
+            self.ret = ((0, 0), (0, 0), 0)
+        return self.ret
+
+
+    def draw(self, img, col="red"):
+
+        comp = 0
+        if col == "blue":
+            comp = (255, 0, 0)
+        elif col == "green":
+            comp = (0, 255, 0)
+        elif col == "red":
+            comp = (0, 0, 255)
+
+        pts = cv2.boxPoints(self.ret)
+        pts = np.int0(pts)
+        img2 = cv2.polylines(img, [pts], True, comp, 2)
+
+        return img2
--- src/tigrillo_tracking/src/tigrillo_tracking/geometry.py
+++ src/tigrillo_tracking/src/tigrillo_tracking/geometry.py
@@ -10,6 +10,8 @@ from geometry_msgs.msg import TransformStamped
 
 import utils
 
+np.set_printoptions(precision=2, suppress=True)
+
 class CustomTF:
 
     def __init__(self, save_tf=True):
@@ -22,59 +24,37 @@ class CustomTF:
         self.save_tf = save_tf
         self.p_world = None
         self.p_cam = None
+        self.M = None
 
-    def __rigid_transform_3D(self):
+    def __rigid_transform_3D(self, A, B):
 
-        assert len(self.p_world) == len(self.p_cam)
+        assert len(A) == len(B)
 
-        N = self.p_cam.shape[0]; # total points
+        N = A.shape[0]; # total points
 
-        centroid_cam = np.mean(self.p_cam, axis=0)
-        centroid_world = np.mean(self.p_world, axis=0)
-
-
+        centroid_A = np.mean(A, axis=0)
+        centroid_B = np.mean(B, axis=0)
+        
         # centre the points
-        P_world = self.p_world - np.tile(centroid_world, (N, 1))
-        P_cam = self.p_cam - np.tile(centroid_cam, (N, 1))
-
-        pickle.dump([P_world, P_cam], open("ici.pkl", "wb"))
-
-        # # Pad matrices
-        # pad = lambda x: np.hstack([x, np.ones((x.shape[0], 1))])
-        # unpad = lambda x: x[:,:-1]
-        # P_world = pad(P_world)
-        # P_cam = pad(P_cam)
-
-        # # Solve least square problem
-        # P_cam = np.hstack((P_cam, np.ones((N, 1))))
-        # print P_cam, P_world.shape
-        # M = np.linalg.lstsq(P_cam, P_world)[0]
-        # print M.shape
-        # R = M[0:3,:]
-        # t = M[3, :]
-        # print R.shape, P_cam[0, 0:3].shape, t.shape
-        # print R.dot(P_cam[0, 0:3]) + t, P_world[0]
-        # print M.dot(P_cam[0, 0:3]), P_world[0]
-
-        # print unpad(np.dot(BB, A))
-        # print unpad(BB)
-        # print unpad(BB) - unpad(np.dot(BB, A))
-        # utils.cleanup()# print AA, BB
+        AA = A - np.tile(centroid_A, (N, 1))
+        BB = B - np.tile(centroid_B, (N, 1))
 
         # dot is matrix multiplication for array
-        H = np.transpose(P_cam).dot(P_world)
+        H = np.transpose(BB) * AA
+
         U, S, Vt = np.linalg.svd(H)
+
         R = Vt.T * U.T
 
         # special reflection case
         if np.linalg.det(R) < 0:
-           ros.logwarn("Reflection detected")
+           print "Reflection detected"
            Vt[2,:] *= -1
            R = Vt.T * U.T
 
-        t = -R.dot(centroid_cam.T) + centroid_world.T
+        t = -R*centroid_B.T + centroid_A.T
+
         M = np.hstack((R, t.reshape((3,1))))
-        print M
 
         # Get error
         reference = self.p_world.T
@@ -82,21 +62,25 @@ class CustomTF:
         reconstruct = M.dot(cam_padded)
         difference = reference - reconstruct
 
-        # reference = unpad(P_world)
-        # reconstruct = unpad(np.dot(A, P_cam.T).T)
-        # difference = reference - reconstruct 
-
-        print reference
-        print reconstruct
-        print difference
+        print "R"
+        print R
+        print "\nt"
+        print t
+        print "\nREFERENCE"
+        print reference.T
+        print "\nRECONSTRUCTION"
+        print reconstruct.T
+        print "\nDIFFERENCE"
+        print difference.T
+        print "\nSTATS: Min, Max, Mean"
         print np.min(difference), np.max(difference), np.mean(difference)
 
-        return M
+        self.M = M
 
-    def __save(self, M):
+    def __save(self):
 
         ros.logwarn("Saving translation and rotation from camera to world coordinates")
-        pickle.dump(M, open(self.save_filename, "wb"))
+        pickle.dump([self.M, self.p_world, self.p_cam], open(self.save_filename, "wb"))
 
     def __load(self):
 
@@ -127,15 +111,14 @@ class CustomTF:
 
         try:
             if load_tf == False:
-                M = self.__rigid_transform_3D()
-                print M
+                self.__rigid_transform_3D(np.mat(self.p_world), np.mat(self.p_cam))
                 if self.save_tf:
-                    self.__save(M)
+                    self.__save()
             else:
-                M = self.__load()
+                [self.M, self.p_world, self.p_cam] = self.__load()
+            
+            return self.M
 
         except Exception, err:
             traceback.print_exc(file=sys.stdout)
             utils.cleanup()
-
-        return M
